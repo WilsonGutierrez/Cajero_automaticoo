050a702ce03d2a054ed8be97a3e9f6205e477291
WilsonGutierrez <60446487+WilsonGutierrez@users.noreply.github.com>
2020-02-05 17:00:24 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace cajeroo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.lblRespuesta = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.btnRetirar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtEfectivo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnPagar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCantidadd = new System.Windows.Forms.TextBox();
+            this.lblCambio = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Location = new System.Drawing.Point(17, 36);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(52, 13);
+            this.lblCantidad.TabIndex = 0;
+            this.lblCantidad.Text = "Cantidad:";
+            // 
+            // lblRespuesta
+            // 
+            this.lblRespuesta.AutoSize = true;
+            this.lblRespuesta.Location = new System.Drawing.Point(181, 39);
+            this.lblRespuesta.Name = "lblRespuesta";
+            this.lblRespuesta.Size = new System.Drawing.Size(70, 13);
+            this.lblRespuesta.TabIndex = 1;
+            this.lblRespuesta.Text = "Respuesta: ?";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(75, 36);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(100, 20);
+            this.txtCantidad.TabIndex = 2;
+            // 
+            // btnRetirar
+            // 
+            this.btnRetirar.Location = new System.Drawing.Point(29, 96);
+            this.btnRetirar.Name = "btnRetirar";
+            this.btnRetirar.Size = new System.Drawing.Size(111, 49);
+            this.btnRetirar.TabIndex = 3;
+            this.btnRetirar.Text = "Retirar";
+            this.btnRetirar.UseVisualStyleBackColor = true;
+            this.btnRetirar.Click += new System.EventHandler(this.btnRetirar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnRetirar);
+            this.groupBox1.Controls.Add(this.lblCantidad);
+            this.groupBox1.Controls.Add(this.txtCantidad);
+            this.groupBox1.Controls.Add(this.lblRespuesta);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(305, 198);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "RETIRAR";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtEfectivo);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.btnPagar);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.txtCantidadd);
+            this.groupBox2.Controls.Add(this.lblCambio);
+            this.groupBox2.Location = new System.Drawing.Point(396, 21);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(333, 189);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "PAGAR";
+            // 
+            // txtEfectivo
+            // 
+            this.txtEfectivo.Location = new System.Drawing.Point(111, 71);
+            this.txtEfectivo.Name = "txtEfectivo";
+            this.txtEfectivo.Size = new System.Drawing.Size(100, 20);
+            this.txtEfectivo.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Efectivo: ";
+            // 
+            // btnPagar
+            // 
+            this.btnPagar.Location = new System.Drawing.Point(29, 122);
+            this.btnPagar.Name = "btnPagar";
+            this.btnPagar.Size = new System.Drawing.Size(111, 49);
+            this.btnPagar.TabIndex = 3;
+            this.btnPagar.Text = "Pagar";
+            this.btnPagar.UseVisualStyleBackColor = true;
+            this.btnPagar.Click += new System.EventHandler(this.btnPagar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cantidad a pagar:";
+            // 
+            // txtCantidadd
+            // 
+            this.txtCantidadd.Location = new System.Drawing.Point(111, 33);
+            this.txtCantidadd.Name = "txtCantidadd";
+            this.txtCantidadd.Size = new System.Drawing.Size(100, 20);
+            this.txtCantidadd.TabIndex = 2;
+            // 
+            // lblCambio
+            // 
+            this.lblCambio.AutoSize = true;
+            this.lblCambio.Location = new System.Drawing.Point(227, 78);
+            this.lblCambio.Name = "lblCambio";
+            this.lblCambio.Size = new System.Drawing.Size(54, 13);
+            this.lblCambio.TabIndex = 1;
+            this.lblCambio.Text = "Cambio: ?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 305);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Cajero Autimático";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.Label lblRespuesta;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Button btnRetirar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtEfectivo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnPagar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCantidadd;
+        private System.Windows.Forms.Label lblCambio;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cajeroo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRetirar_Click(object sender, EventArgs e)
+        {
+            double[] denominaciones = {100,50,20,10,5,1,0.50,0.25,0.10,0.05,0.01 };
+            double cantidad = double.Parse(txtCantidad.Text);
+            sbyte n = 0;
+            String respuesta = "Respuesta: \n";
+            foreach(double denominacion in denominaciones) {
+               while (denominacion <=Math.Round (cantidad,2)) {
+                    n++;
+                    cantidad -= denominacion;
+
+                }
+                if (n > 0) {
+                    respuesta += n + (denominacion > 1 ? " billetes " : " Monedas ")+ denominacion+"\n";
+                }
+                n = 0;
+            }
+            lblRespuesta.Text = respuesta;
+        }
+
+        private void btnPagar_Click(object sender, EventArgs e)
+        {
+            double[] denominaciones = { 100, 50, 20, 10, 5, 1, 0.50, 0.25, 0.10, 0.05, 0.01 };
+            double cantidad = double.Parse(txtCantidadd.Text);
+            double cm = double.Parse(txtEfectivo.Text);
+            sbyte n = 0;
+            double cm2;
+            cm2 = cm - cantidad;
+            String cambio= "Su cambio es de: \n ";
+            foreach(double denominacion in denominaciones)
+            {
+                while (denominacion <= Math.Round(cm2, 2))
+                {
+                    n++;
+                    cm2 -= denominacion;
+
+                }
+                if (n > 0)
+                {
+                    cambio += n + (denominacion > 1 ? " billetes " : " Monedas ") + denominacion + "\n";
+                }
+                n = 0;
+            }
+            lblCambio.Text = cambio;
+        }
+        }
+    }
+
